9c679dc1f3ba3305d49c1a3b92645b38ac3b50b6
daiki01240 <domo01240@gmail.com>
2018-12-29 23:11:23 +0900

    [add] v-function

---
+++ sandbox/test.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        message:'Hello Vue.js!',
+        toggle: false,
+        colors:['Red','Green','Blue'],
+        user:{
+            firstName:'Taro',
+            lastName:'Yamada',
+            age:28
+        }
+    }
+});
