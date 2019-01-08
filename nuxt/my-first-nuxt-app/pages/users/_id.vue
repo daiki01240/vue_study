010cdb2d18a642128623f4d0daedafc9d55f266c
daiki01240 <domo01240@gmail.com>
2019-01-08 22:53:50 +0900

    [add] _id.vue routing

--- nuxt/my-first-nuxt-app/pages/users/_id.vue
+++ nuxt/my-first-nuxt-app/pages/users/_id.vue
@@ -2,8 +2,18 @@
 
   <div>
     <p>
-    /users/_id.vue
+    User ID:{{userId}}
     </p>
   </div>
 
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      userId: this.$route.params.id
+    }
+  }
+}
+</script>
